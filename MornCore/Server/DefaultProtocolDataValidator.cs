07602d1915640e34c6f096715a560ba78e45c02b
fc <morningf@foxmail.com>
2018-02-25 21:28:07 +0800

    添加一个默认数据校验器

---
+++ MornCore/Server/DefaultProtocolDataValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MornCore.Server
+{
+    class DefaultProtocolDataValidator : IProtocolDataVerifiable
+    {
+        public bool VerifyAppKey { get; set; }
+        public bool VerifySignature { get; set; }
+        public bool VerifyAuthority { get; set; }
+
+        public void Validate(MornRequestData data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
